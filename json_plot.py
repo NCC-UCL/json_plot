e693f4a1becc45d3055148886d416cd01bf85d3a
Ross Dobson <56548895+ross-dobson@users.noreply.github.com>
2022-04-26 02:10:13 +0100

    Working plots - tick labels still need some work

--- json_plot.py
+++ json_plot.py
@@ -2,24 +2,14 @@ import json
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import pandas as pd
 
 # TODO: this needs considerable optimisation and legibility improvements
 # TODO: calculating mean/average triggered bugs if >1 runs
 
-class Run():
-    def __init__(self, fuzzer, target, program, run, reached, triggered):
-        self.fuzzer = fuzzer
-        self.target = target
-        self.program = program
-        self.run = run
-        self.reached = reached
-        self.triggered = triggered
-
-    def __str__(self):
-         return "{0} {1} {2} run #{3} | reached {4} triggered {5}".format(self.fuzzer, self.target, self.program, self.run, len(self.reached), len(self.triggered))
-
-def generate_bugplot(json_path):
-    """Triggered vs target"""
+def generate_bugplot(json_path, fig_path):
+    """Read JSON summary file, save plot to fig_path"""
     
     with open(json_path, 'r') as json_file:
         data_str = json_file.read()
@@ -48,73 +38,77 @@ def generate_bugplot(json_path):
     n_fuzzers = len(fuzzers_list)
     print("Found", n_targets, "targets")
     print("Found", n_fuzzers, "fuzzers")
-    
-    
+
+    # Now we create a dataframe, to make life easier when plotting later on
+    df = pd.DataFrame(data=np.zeros(shape=(n_targets,n_fuzzers), dtype=int), index=targets_list, columns=fuzzers_list)
+
+    # Loop through the JSON again to fill in the DF
+    for fuzzer in data['results']:
+        for target in data['results'][fuzzer]:
+            for program in data['results'][fuzzer][target]:
+                for run in data['results'][fuzzer][target][program]:
+                    reached = data['results'][fuzzer][target][program][run]['reached']
+                    triggered = data['results'][fuzzer][target][program][run]['triggered']
+                    df.loc[target,fuzzer] += len(triggered)
+    print("Total results found from", json_path,":")
+    print(df)
+    df = df.replace(to_replace=0, value=0.01)
+
     ###################
     # Create the plot #
     ###################    
-    fig, ax = plt.subplots()
 
-    # we plot n_targets groups of n_fuzzer bars
-    width = 0.85 / n_fuzzers
-    print("WIDTH", width)
-    x = np.arange(n_targets)
-    k = np.arange(n_fuzzers)
-    m = np.ceil( (k+1) /2)
-    n = m * np.power(-1,k)
+    # A lot of this is based on https://stackoverflow.com/questions/59066811/how-can-a-plot-a-5-grouped-bars-bar-chart-in-matplotlib
+    # One day, I'll get round to tweaking this to fix a few remaining undesirable behaviours
+    # Notably the tick labelling being off-centre relative to the bars
+    # TODO: update the tick labelling to more robust method given any number of bars in each group (i.e. any number of fuzzers)
+    # TODO: label each bar with it's numerical value, to make easier than reading off y-axis?
+    fig, ax = plt.subplots(figsize=(16,10))
 
-    print("n:",n)
-    print("x:",x)
+    # Setting the positions and width for the bars
+    pos = list(range(len(df)))
+    num_col = len(df.columns)
+    width = 0.95 / num_col
 
-    if n_fuzzers %2 != 0:  # odd number of bars in groups needs shifting
-        n = n - 0.5
-    
-    # DOBBO so what do I need to be able to plot?
-    # so one line of code per fuzzer, but I need to have all targets ready
-    # ahhh, now I can iterate through all the fuzzers in the JSON again, set all the bars heights to 0 by default
-    # but update if the actual triggered value is found when iterating through all the fuzzers again
-    for i_fuzzer,fuzzer in enumerate(data['results']):
-        print("")
-        print("I, FUZZER",i_fuzzer, fuzzer)
-        bar_heights = np.zeros(n_targets)
-        for i_target, target in enumerate(targets_list):
-            print("")
-            print("i, target",i_target, target)
-            try:
-                for program in data['results'][fuzzer][target]:
-                    for run in data['results'][fuzzer][target][program]:
-                        print("triggered:", data['results'][fuzzer][target][program][run]['triggered'])
-                        bar_heights[i_target] += len(data['results'][fuzzer][target][program][run]['triggered'])
-                        print("ttt:",target,bar_heights[i_target])
-            except:
-                print("Continued past", target)
-                continue
-        # whenever I plot, my position is x-n*(width) or x-n*(width/2)
-        # here we need to do the ax.bar, as it needs to be done PER FUZZER as we need to set the positioning
-        # so here we work out whether we are at width or width/2 depending on odd/even respectively
-        print("ax.bar time!")
-        print("DEBUG", n[i_fuzzer])
-        ax.bar(x + n[i_fuzzer]*(width/2), bar_heights, width, label=fuzzer)
+    bar_labels = df.index
 
-    ax.set_xticks(range(len(targets_list)), targets_list)
+    for i, (colname) in enumerate(df.columns):
+        # 2 = 0.25
+        # 3 = 0.2
+        # 6 = 0.125
+        delta_p = 0.2 + width*i
+        plt.bar([p + delta_p for p in pos],
+            df[colname], width, label=colname)
+
+    ax.set_xticks(pos)
+    def update_ticks(x, pos):
+        return df.index[pos]
+
+    ax.xaxis.set_major_formatter(ticker.NullFormatter())
+    ax.xaxis.set_minor_formatter(ticker.FuncFormatter(update_ticks))
+    ax.xaxis.set_minor_locator(ticker.FixedLocator([p+0.5 for p in pos]))
+    for tick in ax.xaxis.get_minor_ticks():
+        tick.tick1line.set_markersize(0)
+        tick.tick2line.set_markersize(0)
+        tick.label1.set_horizontalalignment('left')
+    ylim = ax.get_ylim()
+    ax.set_ylim(-0.05, ylim[-1])
+
+    # plt.legend()
+    # plt.grid()
+    # plt.show()
     ax.set_title("Mean number of bugs found for each target library")
     ax.set_xlabel("Targets")
     ax.set_ylabel("Number of bugs triggered")
     leg = ax.legend(loc="upper left", bbox_to_anchor=(1.04, 1.0))
-    
-    plt.savefig('bugplot.png', bbox_inches="tight")
-
-
-def generate_sigplot(runs):
-    raise NotImplementedError("Sigplot not yet implemented, sorry!")
-    pass
-
-
+    plt.savefig(fig_path, bbox_inches="tight")
+    print("Plot saved to", fig_path)
 
 
 def main():
-    path = sys.argv[1]
-    generate_bugplot(path)
+    json_path = sys.argv[1]
+    fig_path = sys.argv[2]
+    generate_bugplot(json_path, fig_path)
 
 if __name__ == "__main__":
     main()
